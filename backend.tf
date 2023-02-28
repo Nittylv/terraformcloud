70f0b4640929ebc10552a600b7935263f01bf8bd
Ubuntu <ubuntu@ip-172-31-20-158.ec2.internal>
2023-02-28 09:36:31 +0000

    updated backend file

--- backend.tf
+++ backend.tf
@@ -1,12 +1,12 @@
- terraform {
-   backend "s3" {
-     bucket         = "pbl-test-18"
-     key            = "global/s3/terraform.tfstate"
-     region         = "us-east-1"
-     dynamodb_table = "terraform-locks"
-     encrypt        = true
-   }
- }
+ #terraform {
+ #  backend "s3" {
+ #    bucket         = "pbl-test-18"
+  #   key            = "global/s3/terraform.tfstate"
+   #  region         = "us-east-1"
+   #  dynamodb_table = "terraform-locks"
+   #  encrypt        = true
+  # }
+ #}
 
 
 #terraformcloud is the name of workspace i craeted in Terraform cloud
